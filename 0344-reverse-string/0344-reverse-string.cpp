8022be5d6738275cf67c1dd3f30552a10f54474c
aashika kumari <aashikasharma919@gmail.com>
2026-07-28 12:46:45 +0530

    Time: 0 ms (100%), Space: 27.1 MB (81.69%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int start=0,end=s.size()-1;
-        while(start<end){
-            std::swap(s[start],s[end]);
-                start++;
-                end--;
-        }
+        reverse(s.begin(),s.end());
+
         
     }
 };
